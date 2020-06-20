ef82c659a574e9a330f20469739c5d3ad2ec53e8
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-20 23:43:34 +0530

    Add files via upload

---
+++ P8_Create Integers.py
@@ -0,0 +1,10 @@
+#How to create Integers
+#Integer Object Display
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))  #<class 'int'>
+print(type(y))  #<class 'int'>
+print(type(z))  #<class 'int'>
