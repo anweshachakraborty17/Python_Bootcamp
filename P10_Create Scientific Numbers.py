fb24c5b6bd1bd4bc6a1faccb956ac966d61d3328
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-20 23:56:03 +0530

    Add files via upload

---
+++ P10_Create Scientific Numbers.py
@@ -0,0 +1,9 @@
+#Creat Scientific Numbers with an "e" to indicate the power of 10
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))  #<class 'float'>
+print(type(y))  #<class 'float'>
+print(type(z))  #<class 'float'>
