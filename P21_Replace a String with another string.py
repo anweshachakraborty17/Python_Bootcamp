d570f7580b7563386195189e9633c7bce6a430c9
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-24 23:55:40 +0530

    Add files via upload

---
+++ P21_Replace a String with another string.py
@@ -0,0 +1,7 @@
+#Replace a String with another string
+
+a = "I am Anwesha"
+print(a.replace("Anwesha", "Anushrii")) #I am Anurshrii
+
+b = "I love icecream"
+print(a.replace("e", "i")) #I lovi icicriam
