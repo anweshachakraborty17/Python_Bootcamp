7a6f93ceccd3f5366e16d677df7ad9976a2fcbf2
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-26 23:56:52 +0530

    Python Lists

---
+++ P33_Check if a List item exists.py
@@ -0,0 +1,6 @@
+#Check if a List item exists
+
+thislist = ["Anwesha", "Anushrii", "Jui"]
+if "apple" in thislist:
+  print("Yes, 'Anwesha' is in the names list")  
+  #Yes, 'Anwesha' is in the names list
