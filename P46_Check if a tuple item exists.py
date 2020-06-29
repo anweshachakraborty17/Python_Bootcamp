102f60dfa47c75200b275b097dad588c8a60a241
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-29 23:59:30 +0530

    Python Tuples

---
+++ P46_Check if a tuple item exists.py
@@ -0,0 +1,7 @@
+#check if a tuple item exists
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+  #Yes, 'apple' is in the fruits tuple
