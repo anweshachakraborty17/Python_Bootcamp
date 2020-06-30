f481350123c3bc0c44801ee8d604227d3f1c4dd6
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-30 23:59:33 +0530

    Python Sets

---
+++ P50_Create a set.py
@@ -0,0 +1,8 @@
+#Create a set
+
+thisset = {"apple", "banana", "mango"}
+print(thisset)  #{'apple', 'banana', 'cherry'}
+
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+# Refresh this page to see the change in the result
