08e5774a9ae1adf14891c1116a81f275b9fb89c3
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-30 23:52:16 +0530

    Python Tuples

---
+++ P48_Delete a tuple.py
@@ -0,0 +1,8 @@
+#Delete a tuple
+
+thistuple1 = ("apple", "banana", "mango")
+del thistuple1
+print(thistuple1) #this will raise an error because the tuple no longer exists
+
+#OUTPUT window will show:
+#Traceback (most recent call last): File "./prog.py", line 3, in NameError: name 'thistuple1' is not defined
