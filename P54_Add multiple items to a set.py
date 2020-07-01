2158083c9385645e1d4d49bde2712dc1783a18b3
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-01 23:59:31 +0530

    Python Sets

---
+++ P54_Add multiple items to a set.py
@@ -0,0 +1,7 @@
+#Add multiple items to a set
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.update(["orange", "mango", "grapes"])
+
+print(thisset)  #{'orange', 'apple', 'grapes', 'mango', 'banana', 'cherry'}
