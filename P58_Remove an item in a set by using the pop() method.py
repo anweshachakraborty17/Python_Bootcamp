db02563fa220ea9694cd122e56da8a3ee62e3a5d
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-04 00:05:24 +0530

    Python Sets

---
+++ P58_Remove an item in a set by using the pop() method.py
@@ -0,0 +1,13 @@
+#Remove an item in a set by using the pop() method
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x) #removed item
+
+print(thisset) #the set after removal
+
+
+#apple
+#{'banana', 'cherry'}
