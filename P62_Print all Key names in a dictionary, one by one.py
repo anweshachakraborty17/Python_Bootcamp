279fc53034fe918e64e2264089e96116c7b77a0e
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-04 23:58:24 +0530

    Python Dictionaries

---
+++ P62_Print all Key names in a dictionary, one by one.py
@@ -0,0 +1,13 @@
+#Print all Key names in a dictionary, one by one
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+
+  #brand
+  #model
+  #year
