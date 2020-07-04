1aa006e09b076b57cec5d81ec5d2de3df24404ac
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-04 23:58:03 +0530

    Python Dictionaries

---
+++ P61_Change the value of a specific item in a dictiona.py
@@ -0,0 +1,13 @@
+#Change the value of a specific item in a dictionary
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+thisdict["year"] = 2018
+
+print(thisdict)
+
+#{'brand': 'Ford', 'model': 'Mustang', 'year': 2018}
