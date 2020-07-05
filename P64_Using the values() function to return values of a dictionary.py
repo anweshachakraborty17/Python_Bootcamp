deac0cd3f19dc152d8ff0f4820836da748af8d0d
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-05 23:59:05 +0530

    Python Dictionaries

---
+++ P64_Using the values() function to return values of a dictionary.py
@@ -0,0 +1,13 @@
+#Using the values() function to return values of a dictionary
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+  print(x)
+
+  #Ford
+  #Mustang
+  #1964
