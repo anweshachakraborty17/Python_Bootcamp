b12a4acd99b5b9c66cb6bc1dab09d45bab5aa75f
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-06 00:09:18 +0530

    Python Dictionaries

---
+++ P66_Check if a key exists.py
@@ -0,0 +1,12 @@
+#Check if a key exists
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+
+#Yes, 'model' is one of the keys in the thisdict dictionary
