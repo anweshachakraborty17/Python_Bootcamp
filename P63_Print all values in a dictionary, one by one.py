cfec703822fd52d85e0b1beb6a5b20ca8d7ad0e2
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-05 23:58:41 +0530

    Python Dictionaries

---
+++ P63_Print all values in a dictionary, one by one.py
@@ -0,0 +1,13 @@
+#Print all values in a dictionary, one by one
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
+
+  #Ford
+  #Mustang
+  #1964
