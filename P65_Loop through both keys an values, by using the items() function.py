a2c971023e21dae4e498ab21cd91405132645d0a
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-06 00:08:55 +0530

    Python Dictionaries

---
+++ P65_Loop through both keys an values, by using the items() function.py
@@ -0,0 +1,13 @@
+#Loop through both keys an values, by using the items() function
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
+
+#brand Ford
+#model Mustang
+#year 1964
