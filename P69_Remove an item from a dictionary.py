1efa2f3de660dbd399a35b786e898bedf59a80e8
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-07 23:59:05 +0530

    Python Dictionaries

---
+++ P69_Remove an item from a dictionary.py
@@ -0,0 +1,11 @@
+#Remove an item from a dictionary
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+#{'brand': 'Ford', 'year': 1964}
