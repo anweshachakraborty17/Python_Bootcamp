6c68fea5ce885c2390414655057bd0eb152634fe
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-07 23:58:20 +0530

    Python Dictionaries

---
+++ P67_Get the length of a dictionary.py
@@ -0,0 +1,11 @@
+#Get the length of a dictionary
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+  #Yes, 'model' is one of the keys in the thisdict dictionary
