ef34c38579f2c7a05b47f54dd001ab3e5b386020
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-07-07 23:59:39 +0530

    Python Dictionaries

---
+++ P71_Using the dict() constructor to create a dictiona.py
@@ -0,0 +1,8 @@
+#Using the dict() constructor to create a dictionary
+
+thisdict = dict(brand="Ford", model="Mustang", year=1964)
+# note that keywords are not string literals
+# note the use of equals rather than colon for the assignment
+print(thisdict)
+
+#{'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
