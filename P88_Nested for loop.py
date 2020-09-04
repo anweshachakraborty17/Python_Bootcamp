ff197e7442cd4b14efb9417a21b8e1435eb2dbad
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-09-04 23:25:34 +0530

    Python For Loop

---
+++ P88_Nested for loop.py
@@ -0,0 +1,19 @@
+#Nested for loop
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+
+    
+#red apple
+#red banana
+#red cherry
+#big apple
+#big banana
+#big cherry
+#tasty banana
+#tasty cherry
