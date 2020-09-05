8ca97812fb22093c6a0e5ac74a889440ad905608
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-09-05 23:58:23 +0530

    Python Functions

---
+++ P90_Function parameters.py
@@ -0,0 +1,13 @@
+#Function parameters
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+#Emil Refsnes
+#Tobias Refsnes
+#Linus Refsnes
