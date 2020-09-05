17319b2585966ee1ab9798f9b802a6358512f22b
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-09-05 23:58:53 +0530

    Python Functions

---
+++ P91_Default parameter value.py
@@ -0,0 +1,14 @@
+#Default parameter value
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+#I am from Sweden
+#I am from India
+#I am from Norway
+#I am from Brazil
